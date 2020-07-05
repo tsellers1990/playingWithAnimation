98e6823a160bcc9c1686dccb0d5a030716f2d10e
tsellers1990 <tsellers1990@gmail.com>
2020-07-05 12:17:37 -0600

    fixed animations running multiple times. also added a stop button

--- logic.js
+++ logic.js
@@ -1,36 +1,60 @@
-var boxMoveDown = () =>{
-    var elem = document.getElementById("animationBox");
-    var currentPosition = animationBox.style.cssText;
-    var pos = 0;
-    var id = setInterval(frameLower, 10);
+var isClickable = true;
+var pos = 0;
+var stopped = false;
+
+const stopMove = () => {
+    stopped = true
+}
+
+const boxMoveDown = () =>{
+    if (isClickable){
+        stopped = false
+        var elem = document.getElementById("animationBox");
+        pos = this.pos
+        var id = setInterval(frameLower, 10);
+        
+        
+        function frameLower() {
+            if (pos >= 350) { //if the position is @ the end location
+                isClickable = true;
+                clearInterval(id); //stop the interval
     
-    
-    function frameLower() {
-        if (pos == 350) { //if the position is @ the end location
-            clearInterval(id); //stop the interval
-        } else{
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos+ 'px';
+            } else if(stopped){
+                clearInterval(id);
+                isClickable = true;
+            } else{
+                isClickable = false;
+                pos++;
+                elem.style.top = pos + 'px';
+                elem.style.left = pos+ 'px';
+            }
         }
     }
     
 }
 
-var boxMoveUp = () => {
-    var elem = document.getElementById("animationBox");
-    var currentPosition = animationBox.style.cssText;
-    var pos = 350;
-    var id = setInterval(frameUpper, 10);
-    
-    
-    function frameUpper() {
-        if (pos == 0) { //if the position is @ the end location
-            clearInterval(id); //stop the interval
-        } else{
-            pos--;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos+ 'px';
+const boxMoveUp = () => {
+    if(isClickable && stopped){
+        stopped = false
+        var elem = document.getElementById("animationBox");
+        // var pos = 350;
+        pos = this.pos;
+        var id = setInterval(frameUpper, 10);
+        
+        
+        function frameUpper() {
+            if (pos == 0) { //if the position is @ the end location
+                isClickable = true;
+                clearInterval(id); //stop the interval
+            } else if(stopped){
+                clearInterval(id);
+                isClickable = true;
+            } else{
+                isClickable = false;
+                pos--;
+                elem.style.top = pos + 'px';
+                elem.style.left = pos+ 'px';    
+            }
         }
     }
 }
